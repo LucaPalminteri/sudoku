86767044e24e2c87c0032443ec81d2b94a92e088
LucaPalminteri <lucapalminteri02@gmail.com>
2024-09-17 22:31:18 -0300

    feat: add disabled option button component

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -1,8 +1,8 @@
-const createButton = (text = "", onClick = null, className = "", style = {}) => {
+const createButton = (text = "", onClick = null, className = "", style = {}, disabled = false) => {
   const button = window.document.createElement("button");
   button.textContent = text;
 
-  if (onClick) {
+  if (onClick && !disabled) {
     button.addEventListener("click", onClick);
   }
 
@@ -12,6 +12,11 @@ const createButton = (text = "", onClick = null, className = "", style = {}) => 
 
   if (Object.keys(style).length > 0) {
     Object.assign(button.style, style);
+  }
+
+  if (disabled) {
+    button.disabled = true;
+    button.classList.add("disabled");
   }
 
   return button;
